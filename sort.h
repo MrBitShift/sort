2aeb4c24a0f4f4e1a87a3ba6330fd72a3cf918f0
MrBitShift <44534752+MrBitShift@users.noreply.github.com>
2019-03-24 20:00:00 -0700

    v2.0.0
    Improved error reporting system.
    
    Removed unnecessary files.

--- sort.h
+++ sort.h
@@ -1,15 +1,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "dbg.h"
+
+char *sort_err = "None";
+
+#define check_err(A, B) if (!(A)) {\
+	sort_err = B;\
+	goto error;\
+}
+
+#define sort_get_err() (sort_err)
 
 int lower(char *in, size_t len, char **out)
 {
 	int i;
 	
-	check(in != NULL, "in can't be null.");
-	check(len > 0, "len must be greater than 0.");
-	check(out != NULL, "out cannot be null.");
+	check_err(in != NULL, "in can't be null.");
+	check_err(len > 0, "len must be greater than 0.");
+	check_err(out != NULL, "out cannot be null.");
 
 	*out = calloc(len + 1, sizeof(char));
 
@@ -18,10 +26,10 @@ int lower(char *in, size_t len, char **out)
 		(*out)[i] = tolower(in[i]);
 	}
 
-	return OPERATION_SUCCESS;
+	return 0;
 
 error:
-	return OPERATION_FAILURE;
+	return 1;
 }
 
 int int_insertion_sort(int *arr, size_t arr_len, int **out)
@@ -30,9 +38,9 @@ int int_insertion_sort(int *arr, size_t arr_len, int **out)
 	int b;
 	int current;
 
-	check(arr != NULL, "arr can't be null.");
-	check(arr_len > 0, "len must be greater than 0.");
-	check(out != NULL, "out must not be null.");
+	check_err(arr != NULL, "arr can't be null.");
+	check_err(arr_len > 0, "len must be greater than 0.");
+	check_err(out != NULL, "out must not be null.");
 
 	*out = calloc(arr_len, sizeof(int));
 	memcpy(*out, arr, arr_len * sizeof(int));
@@ -48,10 +56,10 @@ int int_insertion_sort(int *arr, size_t arr_len, int **out)
 		(*out)[b + 1] = current;
 	}
 
-	return OPERATION_SUCCESS;
+	return 0;
 
 error:
-	return OPERATION_FAILURE;
+	return 1;
 }
 
 int char_insertion_sort(char *arr, char **out)
@@ -61,9 +69,9 @@ int char_insertion_sort(char *arr, char **out)
 	char current;
 	size_t arr_len = strlen(arr);
 
-	check(arr != NULL, "arr can't be null.");
-	check(arr_len > 0, "len must be greater than 0.");
-	check(out != NULL, "out must not be null.");
+	check_err(arr != NULL, "arr can't be null.");
+	check_err(arr_len > 0, "len must be greater than 0.");
+	check_err(out != NULL, "out must not be null.");
 
 	*out = calloc(arr_len + 1, sizeof(char)); // + 1 for null
 	memcpy(*out, arr, arr_len * sizeof(char));
@@ -79,10 +87,10 @@ int char_insertion_sort(char *arr, char **out)
 		(*out)[b + 1] = current;
 	}
 
-	return OPERATION_SUCCESS;
+	return 0;
 
 error:
-	return OPERATION_FAILURE;
+	return 1;
 }
 
 int string_insertion_sort(char **arr, size_t arr_len, char ***out)
@@ -92,10 +100,10 @@ int string_insertion_sort(char **arr, size_t arr_len, char ***out)
 	int b;
 	char *current;
 
-	check(arr != NULL, "arr can't be null.");
-	check(*arr != NULL, "members of arr can't be null.");
-	check(arr_len > 0, "arr_len must be greater than 0.");
-	check(out != NULL, "out must not be null.");
+	check_err(arr != NULL, "arr can't be null.");
+	check_err(*arr != NULL, "members of arr can't be null.");
+	check_err(arr_len > 0, "arr_len must be greater than 0.");
+	check_err(out != NULL, "out must not be null.");
 
 	*out = calloc(arr_len, sizeof(char*));
 	memcpy(*out, arr, arr_len * sizeof(char*));
@@ -115,10 +123,10 @@ int string_insertion_sort(char **arr, size_t arr_len, char ***out)
 		(*out)[b + 1] = current;
 	}
 
-	return OPERATION_SUCCESS;
+	return 0;
 
 error:
-	return OPERATION_FAILURE;
+	return 1;
 }
 
 int string_lex_insertion_sort(char **arr, size_t arr_len, char ***out)
@@ -128,10 +136,10 @@ int string_lex_insertion_sort(char **arr, size_t arr_len, char ***out)
 	char **lower_case;
 	char **compare_lower;
 
-	check(arr != NULL, "arr can't be null.");
-	check(*arr != NULL, "members of arr can't be null.");
-	check(arr_len > 0, "arr_len must be greater than 0.");
-	check(out != NULL, "out must not be null.");
+	check_err(arr != NULL, "arr can't be null.");
+	check_err(*arr != NULL, "members of arr can't be null.");
+	check_err(arr_len > 0, "arr_len must be greater than 0.");
+	check_err(out != NULL, "out must not be null.");
 
 	*out = calloc(arr_len, sizeof(char*));
 	memcpy(*out, arr, arr_len * sizeof(char*));
@@ -140,13 +148,13 @@ int string_lex_insertion_sort(char **arr, size_t arr_len, char ***out)
 	{
 		char* current = (*out)[i];
 		lower_case = calloc(1, sizeof(char*));
-		check(lower(current, strlen(current), lower_case) == 0,
+		check_err(lower(current, strlen(current), lower_case) == 0,
 			"failure in lower.");
 		
 		for (b = i - 1; b >= 0; b--)
 		{
 			compare_lower = calloc(1, sizeof(char*));
-			check(lower(
+			check_err(lower(
 				(*out)[b], strlen((*out)[b]), compare_lower)
 				== 0,
 				"failure in lower.");
@@ -164,7 +172,7 @@ int string_lex_insertion_sort(char **arr, size_t arr_len, char ***out)
 	free(*compare_lower);
 	free(compare_lower);
 
-	return OPERATION_SUCCESS;
+	return 0;
 
 error:
 	free(*lower_case);
@@ -172,5 +180,5 @@ error:
 	free(*compare_lower);
 	free(compare_lower);
 
-	return OPERATION_FAILURE;
+	return 1;
 }
